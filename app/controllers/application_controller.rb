db73e9b60fb4c9c65520f063f8a9ed6aeacf54ec
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-08-07 16:04:12 +0900

    Basic認証のエラーを修正&サインアップ機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_full_width])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_full_width])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kana_input])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana_input])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+  end
 
   private
 
@@ -7,5 +21,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username,password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
-  end'
+  end
 end
