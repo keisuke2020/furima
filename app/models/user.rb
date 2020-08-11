d40c307bdc055e25634b0d8d35b87f5286eeb5b4
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-08-11 14:22:04 +0900

    userモデルのバリデーションをwith_optionsを用いて簡略化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :password, presence: true, length: { minimum: 6 },
-  format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
-  validates :family_name_full_width, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :first_name_full_width, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :family_name_kana_input, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :first_name_kana_input, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :birthday, presence: true
+  zenkaku = /\A[ぁ-んァ-ン一-龥]/
+  kana = /\A[ァ-ヶー－]+\z/
 
+  with_options presence: true do
+    validates :nickname
+    validates :password, length: { minimum: 6 },
+                         format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
+    validates :family_name_full_width, format: { with: zenkaku, message: "is invalid. Input full-width characters."}
+    validates :first_name_full_width, format: { with: zenkaku, message: "is invalid. Input full-width characters."}
+    validates :family_name_kana_input, format: { with: kana, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana_input, format: { with: kana, message: "is invalid. Input full-width katakana characters."}
+    validates :birthday
+  end
 end
