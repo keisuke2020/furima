611b4756b699f64801a67bd512f10c7359f0f81e
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-08-11 13:20:40 +0900

    テストコードに「ユーザー登録がうまくいく時」を追加実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,37 @@ RSpec.describe User, type: :model do
     before do
        @user = FactoryBot.build(:user)
     end
+    context '新規登録がうまくいく時' do
+      it 'nickname,email,password,password_confirmation,
+      family_name_full_width,first_name_full_width,family_name_kana_input,
+      first_name_kana_input,birthdayが全て存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    
+      it 'emailは@を含めば登録できる' do
+        @user.email = "furima@co.jp"
+        expect(@user).to be_valid
+      end
+
+      it 'passwordが6文字以上であれば登録できる' do
+        @user.password = "000000"
+        @user.password_confirmation = "000000"
+        expect(@user).to be_valid
+      end
+
+      it 'family_name_full_widthとfirst_name_full_widthが全角であれば登録できる' do
+        @user.family_name_full_width = 'フリマ'
+        @user.first_name_full_width = '四郎'
+        expect(@user).to be_valid
+      end
+
+      it 'family_name_kana_inputとfirst_name_kana_inputが全角カタカナ入力であれば登録できる' do
+        @user.family_name_kana_input = 'フリマ'
+        @user.first_name_kana_input = 'シロウ'
+        expect(@user).to be_valid
+      end
+    end
+
     context '新規登録がうまくいかないとき' do
       it "nicknameが空では登録できないこと" do
         @user.nickname = nil
